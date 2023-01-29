4b60d6a99adb2878e2d18c6296685964dbe3f0a6
Matti Luukkainen <mluukkai@iki.fi>
2023-01-29 22:55:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 const validator = (request, response, next) => {
+  console.log()
+
   const { content } = request.body
 
-  if (!content || content.length<5 ) {
+  if (request.method==='POST' && (!content || content.length<5) ) {
     return response.status(400).json({
       error: 'too short anecdote, must have length 5 or more'
     })
