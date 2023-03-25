ed36ccf883ad8cd1e154020bf5f91f17d49ede2d
vkats90 <mars77@gmail.com>
2023-03-25 17:41:32 -0600

    exercise 1.1 complete

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,41 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.parts[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.parts[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.parts[2]} {props.exercises[2]}
+      </p>
+    </>
+  );
+};
+const Total = (props) => {
+  return <p>Number of exercises {props.exercises.reduce((a, b) => a + b)}</p>;
+};
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    "Fundamentals of React",
+    "Using props to pass data",
+    "State of a component",
+  ];
+  const exercises = [10, 7, 14];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
+    </div>
+  );
+};
 
 export default App;
