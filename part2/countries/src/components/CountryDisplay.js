ad2091c1b7cdf460b88dfd8a275ba0e6248b5e4d
vkats90 <mars77@gmail.com>
2023-04-06 09:03:28 -0600

    exercise 2.19 complete

--- part2/countries/src/components/CountryDisplay.js
+++ part2/countries/src/components/CountryDisplay.js
@@ -1,23 +1,33 @@
+import OneCountry from "./OneCountry";
+import { useState } from "react";
+
 const CountryDisplay = ({ cntry }) => {
+  const [current, setCurrent] = useState();
+
+  const handleClick = (x) => () => {
+    current && x.name.common === current.name.common
+      ? setCurrent()
+      : setCurrent(x);
+  };
+
   if (cntry.length > 10) return <p>Too many countries, be more specific</p>;
   if (cntry.length === 0) return <p>No countries found</p>;
-  if (cntry.length > 1)
-    return cntry.map((x) => <li key={x.coic}>{x.name.common}</li>);
-  if (cntry.length === 1)
-    return (
-      <>
-        <h2>{cntry[0].name.common}</h2>
-        <p>Capital: {cntry[0].capital[0]}</p>
-        <p>Area: {cntry[0].area} sqkm</p>
-        <h3>Languages:</h3>
-        <ul>
-          {Object.values(cntry[0].languages).map((x) => (
-            <li>{x}</li>
-          ))}
-        </ul>
-        <img src={cntry[0].flags.png} width="200" alt="countryflag" />
-      </>
-    );
+  if (cntry.length > 1) {
+    return cntry.map((x) => (
+      <li key={x.coic}>
+        {x.name.common}{" "}
+        <button onClick={handleClick(x)}>
+          {current && x.name.common === current.name.common ? "hide" : "show"}
+        </button>
+        {current && x.name.common === current.name.common ? (
+          <OneCountry country={current} />
+        ) : (
+          <></>
+        )}
+      </li>
+    ));
+  }
+  if (cntry.length === 1) return <OneCountry country={cntry[0]} />;
 };
 
 export default CountryDisplay;
---
+++ part2/countries/src/components/OneCountry.js
@@ -0,0 +1,18 @@
+const OneCountry = ({ country }) => {
+  return (
+    <>
+      <h2>{country.name.common}</h2>
+      <p>Capital: {country.capital[0]}</p>
+      <p>Area: {country.area} sqkm</p>
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((x) => (
+          <li>{x}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} width="200" alt="countryflag" />
+    </>
+  );
+};
+
+export default OneCountry;
