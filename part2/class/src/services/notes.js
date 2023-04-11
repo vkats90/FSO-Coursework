a305378a679c6b8b1789a70c2539fc5071507a80
vkats90 <mars77@gmail.com>
2023-04-10 18:30:28 -0600

    made a production build for the notes app

--- part2/class/src/services/notes.js
+++ part2/class/src/services/notes.js
@@ -1,15 +1,9 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/notes";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  const nonExisting = {
-    id: 10000,
-    content: "This note is not saved to server",
-    important: true,
-  };
-  return request.then((response) => response.data.concat(nonExisting));
-  //return request.then((response) => response.data);
+  return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -85,6 +88,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
