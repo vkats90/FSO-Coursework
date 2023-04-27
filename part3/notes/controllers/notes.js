bdb43ca839aa93d9f38b6816792e6311a82f6c55
vkats90 <mars77@gmail.com>
2023-04-26 20:22:20 -0600

    followed course

--- part3/notes/controllers/notes.js
+++ part3/notes/controllers/notes.js
@@ -2,9 +2,11 @@
 
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -17,15 +19,33 @@ notesRouter.get("/:id", async (request, response) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
