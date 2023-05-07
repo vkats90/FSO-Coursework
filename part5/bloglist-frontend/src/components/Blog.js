d141cab2d3cd371a375087c20f335c402e966aaa
vkats90 <mars77@gmail.com>
2023-05-07 10:24:40 -0600

    exercise 5.7 complete

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} -{" "}
-    <span style={{ fontStyle: "italic", color: "gray" }}>{blog.author}</span>
-  </div>
-);
+import { useState } from "react";
+const blogStyle = {
+  border: "1px solid black",
+  borderRadius: 5,
+  padding: 5,
+  width: 500,
+  margin: 5,
+};
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div style={blogStyle}>
+      {blog.title}{" "}
+      <span style={{ fontStyle: "italic", color: "gray" }}>
+        -{blog.author}{" "}
+      </span>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? "hide" : "show"}
+      </button>
+      <div style={{ display: visible ? "none" : "" }}></div>
+      <div style={{ display: visible ? "" : "none" }}>
+        <div>{blog.url}</div>
+        likes: {blog.likes} <button>like</button> <br />
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
