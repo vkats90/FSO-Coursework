6e972c75459b98ae515910352d9121d1635529d5
vkats90 <mars77@gmail.com>
2023-05-29 22:10:52 -0600

    exercise 6.3 complete

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,7 +4,7 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -13,17 +13,29 @@ const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
+    votes: 0,
   }
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+export const addVote = (id) => {
+  return {
+    type: 'ADD_VOTE',
+    payload: { id },
+  }
+}
 
-  return state
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_VOTE':
+      return state.map((x) => {
+        if (x.id === action.payload.id) x.votes += 1
+        return x
+      })
+    default:
+      return state
+  }
 }
 
 export default reducer
