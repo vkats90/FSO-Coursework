a69aa7ddef2a82383a85247f014907b2cd4d514a
vkats90 <mars77@gmail.com>
2023-06-10 07:24:33 -0400

    exercise 6.20 complete

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteForm from "./components/AnecdoteForm"
+import Notification from "./components/Notification"
+import { useQuery } from "react-query"
+import { getAll } from "./requests"
 
 const App = () => {
+	const handleVote = (anecdote) => {
+		console.log("vote")
+	}
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+	const result = useQuery("anecdotes", getAll, {
+		refetchOnWindowFocus: false,
+		retry: 1,
+	})
+	console.log(result)
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+	if (result.isLoading) return <div>Loading..</div>
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
-  )
+	if (result.isError)
+		return <div>Couldn't load data due to error: {result.error.message}</div>
+
+	const anecdotes = result.data
+
+	return (
+		<div>
+			<h3>Anecdote app</h3>
+
+			<Notification />
+			<AnecdoteForm />
+
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote)}>vote</button>
+					</div>
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default App
