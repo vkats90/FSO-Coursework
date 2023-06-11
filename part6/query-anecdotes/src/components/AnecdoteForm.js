8cc5c4ed5e01a6078e6622e8b659044a938eb662
vkats90 <mars77@gmail.com>
2023-06-11 12:05:37 -0400

    exercise 6.21 complete

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,32 @@
+import { addAnecdote } from "../requests"
+import { useMutation, useQueryClient } from "react-query"
+
 const AnecdoteForm = () => {
+	const queryClient = useQueryClient()
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+	const anecdoteMutation = useMutation(addAnecdote, {
+		onSuccess: (newAnecdote) => {
+			const anecdotes = queryClient.getQueryData("anecdotes")
+			queryClient.setQueryData("anecdotes", anecdotes.concat(newAnecdote))
+		},
+	})
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
+	const onCreate = (event) => {
+		event.preventDefault()
+		const content = event.target.anecdote.value
+		event.target.anecdote.value = ""
+		anecdoteMutation.mutate({ content, votes: 0 })
+	}
+
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name="anecdote" />
+				<button type="submit">create</button>
+			</form>
+		</div>
+	)
 }
 
 export default AnecdoteForm
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -3,3 +3,6 @@ import axios from "axios"
 const baseUrl = "http://localhost:3001/anecdotes"
 
 export const getAll = () => axios.get(baseUrl).then((res) => res.data)
+
+export const addAnecdote = (anecdote) =>
+	axios.post(baseUrl, anecdote).then((res) => res.data)
