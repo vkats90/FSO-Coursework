da13d331feee1f5b926bb6395cc425fc9bab118d
vkats90 <mars77@gmail.com>
2023-07-07 12:54:10 -0600

    added blog comment functionality

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,72 +1,68 @@
-const blogRouter = require("express").Router();
-const Blog = require("../models/blog");
-const { userExtract } = require("../utils/middleware");
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+const { userExtract } = require('../utils/middleware')
 
-blogRouter.get("/", async (request, response, next) => {
+blogRouter.get('/', async (request, response, next) => {
   try {
-    const blogs = await Blog.find({}).populate("user", {
+    const blogs = await Blog.find({}).populate('user', {
       username: 1,
       name: 1,
-    });
-    response.json(blogs);
+    })
+    response.json(blogs)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-blogRouter.post("/", userExtract, async (request, response, next) => {
-  const user = request.user;
+blogRouter.post('/', userExtract, async (request, response, next) => {
+  const user = request.user
 
-  const blog = new Blog(request.body);
-  blog.user = user.id;
+  const blog = new Blog(request.body)
+  blog.user = user.id
 
   try {
-    const result = await blog.save();
-    user.blogs.push(result.id);
-    await user.save();
-    response.status(201).json(result);
+    const result = await blog.save()
+    user.blogs.push(result.id)
+    await user.save()
+    response.status(201).json(result)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-blogRouter.delete("/:id", userExtract, async (request, response, next) => {
+blogRouter.delete('/:id', userExtract, async (request, response, next) => {
   try {
-    const user = request.user;
-    const blog = await Blog.findById(request.params.id);
+    const user = request.user
+    const blog = await Blog.findById(request.params.id)
     if (!blog) {
-      return response
-        .status(400)
-        .json({ error: "blog with this id does not exist" });
+      return response.status(400).json({ error: 'blog with this id does not exist' })
     }
     if (blog.user.toString() !== user.id.toString()) {
-      return response
-        .status(401)
-        .json({ error: "unauthorized, you did not create this blog" });
+      return response.status(401).json({ error: 'unauthorized, you did not create this blog' })
     }
-    await Blog.findByIdAndRemove(request.params.id);
-    response.status(204).json({ success: `Blog ${request.params.id} removed` });
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).json({ success: `Blog ${request.params.id} removed` })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-blogRouter.put("/:id", userExtract, async (request, response, next) => {
+blogRouter.put('/:id', userExtract, async (request, response, next) => {
   try {
-    const { title, author, url, likes } = request.body;
-    blog = await Blog.findByIdAndUpdate(
+    const { title, author, url, likes, comments } = request.body
+    const blog = await Blog.findByIdAndUpdate(
       request.params.id,
-      { title, author, url, likes },
+      { title, author, url, likes, comments },
       {
         new: true,
         runValidators: true,
-        context: "query",
+        context: 'query',
       }
-    );
-    response.json(blog);
+    )
+    response.json(blog)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-module.exports = blogRouter;
+module.exports = blogRouter
--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -1,28 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, "Blog title is required"],
+    required: [true, 'Blog title is required'],
   },
   author: String,
   url: {
     type: String,
-    required: [true, "URL is required"],
+    required: [true, 'URL is required'],
   },
   likes: { type: Number, default: 0 },
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: 'User',
   },
-});
+  comments: [
+    {
+      type: String,
+    },
+  ],
+})
 
-blogSchema.set("toJSON", {
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Blog", blogSchema);
+module.exports = mongoose.model('Blog', blogSchema)
