1acb1f699e741a913defbdc49a8e97eeb3bfb3e3
vkats90 <mars77@gmail.com>
2023-07-13 14:04:25 -0600

    exercise 8.8 complete

---
+++ part8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,41 @@
+import { ALL_AUTHORS } from '../queries'
+import { useQuery } from '@apollo/client'
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null
+  }
+  const res = useQuery(ALL_AUTHORS)
+
+  if (res.loading) {
+    return <div>loading...</div>
+  }
+
+  const authors = res.data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th width={100}>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td width={100} style={{ textAlign: 'center' }}>
+                {a.born}
+              </td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
