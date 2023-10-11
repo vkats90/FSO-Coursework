b893aa86f83fc221cba2673bc48ff06cf24d09b3
vkats90 <mars77@gmail.com>
2023-10-11 11:03:55 -0600

    connecting to the database

--- part8/phonebook-graphql-backend/index.js
+++ part8/phonebook-graphql-backend/index.js
@@ -1,12 +1,10 @@
 const { WebSocketServer } = require('ws')
 const { useServer } = require('graphql-ws/lib/use/ws')
-
+require('dotenv').config()
 const { ApolloServer } = require('@apollo/server')
 
 const { expressMiddleware } = require('@apollo/server/express4')
-const {
-	ApolloServerPluginDrainHttpServer,
-} = require('@apollo/server/plugin/drainHttpServer')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 const express = require('express')
 const cors = require('cors')
@@ -21,76 +19,69 @@ const User = require('./models/user')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 
-const MONGODB_URI = 'mongodb+srv://databaseurlhere'
+const MONGODB_URI = process.env.MONGODB_URI
 
 console.log('connecting to', MONGODB_URI)
 
 mongoose
-	.connect(MONGODB_URI)
-	.then(() => {
-		console.log('connected to MongoDB')
-	})
-	.catch((error) => {
-		console.log('error connection to MongoDB:', error.message)
-	})
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 // setup is now within a function
 const start = async () => {
-	const app = express()
-	const httpServer = http.createServer(app)
+  const app = express()
+  const httpServer = http.createServer(app)
 
-	const wsServer = new WebSocketServer({
-		server: httpServer,
-		path: '/',
-	})
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/',
+  })
 
-	const schema = makeExecutableSchema({ typeDefs, resolvers })
-	const serverCleanup = useServer({ schema }, wsServer)
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const serverCleanup = useServer({ schema }, wsServer)
 
-	const server = new ApolloServer({
-		schema: makeExecutableSchema({ typeDefs, resolvers }),
-		plugins: [
-			ApolloServerPluginDrainHttpServer({ httpServer }),
-			{
-				async serverWillStart() {
-					return {
-						async drainServer() {
-							await serverCleanup.dispose()
-						},
-					}
-				},
-			},
-		],
-	})
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose()
+            },
+          }
+        },
+      },
+    ],
+  })
 
-	await server.start()
+  await server.start()
 
-	app.use(
-		'/',
-		cors(),
-		express.json(),
-		expressMiddleware(server, {
-			context: async ({ req }) => {
-				const auth = req ? req.headers.authorization : null
-				if (auth && auth.startsWith('Bearer ')) {
-					const decodedToken = jwt.verify(
-						auth.substring(7),
-						process.env.JWT_SECRET
-					)
-					const currentUser = await User.findById(decodedToken.id).populate(
-						'friends'
-					)
-					return { currentUser }
-				}
-			},
-		})
-	)
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+          const currentUser = await User.findById(decodedToken.id).populate('friends')
+          return { currentUser }
+        }
+      },
+    })
+  )
 
-	const PORT = 4000
+  const PORT = 4000
 
-	httpServer.listen(PORT, () =>
-		console.log(`Server is now running on http://localhost:${PORT}`)
-	)
+  httpServer.listen(PORT, () => console.log(`Server is now running on http://localhost:${PORT}`))
 }
 
 start()
