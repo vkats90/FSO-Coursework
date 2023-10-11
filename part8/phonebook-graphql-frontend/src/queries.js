13ac9e88e94fa23088d6ec1e05734576289dfe23
vkats90 <mars77@gmail.com>
2023-10-11 12:15:01 -0600

    update the frontend to support subsciptions

--- part8/phonebook-graphql-frontend/src/queries.js
+++ part8/phonebook-graphql-frontend/src/queries.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client'
 
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone
+    address {
+      street
+      city
+    }
+  }
+`
+
 export const EDIT_NUMBER = gql`
   mutation editNumber($name: String!, $phone: String!) {
     editNumber(name: $name, phone: $phone) {
@@ -27,15 +39,21 @@ export const ALL_PERSONS = gql`
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+
+  ${PERSON_DETAILS}
+`
+
+export const PERSON_ADDED = gql`
+  subscription {
+    personAdded {
+      ...PersonDetails
+    }
+  }
+
+  ${PERSON_DETAILS}
 `
 
 export const CREATE_PERSON = gql`
