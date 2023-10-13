c88eafb73729670ea02f451e459b5fa5d35d318a
vkats90 <mars77@gmail.com>
2023-10-13 15:18:26 -0600

    exercise 9.1 complete, bmi calculator

---
+++ part9/ts-exercises/bmiCalculator.ts
@@ -0,0 +1,23 @@
+const calculateBmi = (height: number, weight: number): string => {
+  let bmi: number = weight / ((height / 100) ^ 2);
+  switch (true) {
+    case bmi < 16:
+      return "Underweight (Severe thinness)";
+    case bmi >= 16 && bmi <= 16.9:
+      return "Underweight (Moderate thinness)";
+    case bmi >= 17 && bmi <= 18.4:
+      return "Underweight (Mild thinness)";
+    case bmi >= 18.5 && bmi <= 24.9:
+      return "Normal (healthy weight)";
+    case bmi >= 25 && bmi <= 29.9:
+      return "Overweight (Pre-obese)";
+    case bmi >= 30 && bmi <= 34.9:
+      return "Obese (Class I)";
+    case bmi >= 35 && bmi <= 39.9:
+      return "Obese (Class II)";
+    case bmi >= 40:
+      return "Obese (Class III)";
+  }
+};
+
+console.log(calculateBmi(180, 74));
