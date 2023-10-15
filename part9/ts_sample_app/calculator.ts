3dbc6b724ec5681b31901df438f894d98f88e747
vkats90 <mars77@gmail.com>
2023-10-15 17:09:23 -0600

    followed course material

--- part9/ts_sample_app/calculator.ts
+++ part9/ts_sample_app/calculator.ts
@@ -1,6 +1,6 @@
-type Operation = "multiply" | "add" | "divide";
+export type Operation = "multiply" | "add" | "divide";
 
-const calculator = (a: number, b: number, op: Operation): number => {
+export const calculator = (a: number, b: number, op: Operation): number => {
   switch (op) {
     case "multiply":
       return a * b;
--- part9/ts_sample_app/index.ts
+++ part9/ts_sample_app/index.ts
@@ -1,8 +1,20 @@
+import { calculator, Operation } from "./calculator";
+
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 app.get("/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.post("/calculate", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+  res.send({ result });
 });
 
 const PORT = 3003;
