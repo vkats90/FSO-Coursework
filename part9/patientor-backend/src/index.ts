ecf98497d4eea4246922635ca0f745b5935fff2f
vkats90 <mars77@gmail.com>
2023-10-22 11:35:34 -0600

    completed exercise 9.10, structures and added api/diagnoses endpoint

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import router from './routes/diagnoses'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use('/api/diagnoses', router)
 
 app.get('/api/ping', (_req, res) => {
   console.log('something is pinging the server')
---
+++ part9/patientor-backend/src/services/diagnosesService.ts
@@ -0,0 +1,8 @@
+import diagnosisData from '../../data/diagnoses'
+import { Diagnosis } from '../types'
+
+const getDiagnoses = (): Diagnosis[] => {
+  return diagnosisData
+}
+
+export default { getDiagnoses }
