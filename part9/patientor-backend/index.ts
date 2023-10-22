cdd7564df55191d161a26397208b2f071fdc6ae8
vkats90 <mars77@gmail.com>
2023-10-21 18:57:41 -0600

    exercise 9.9 complete, backend works with frontend

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,14 +1,17 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('something is pinging the server')
   res.send('pong')
 })
 
-const PORT = 3000
+const PORT = 3001
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
