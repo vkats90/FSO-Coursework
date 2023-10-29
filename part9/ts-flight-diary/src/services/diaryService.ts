4d93f03d15a123da2c6fb10448635f697977074c
vkats90 <mars77@gmail.com>
2023-10-29 12:52:57 -0600

    followed course material

--- part9/ts-flight-diary/src/services/diaryService.ts
+++ part9/ts-flight-diary/src/services/diaryService.ts
@@ -1,6 +1,6 @@
 import diaryData from '../../data/entries'
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types'
+import { NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry } from '../types'
 
 const diaries: DiaryEntry[] = diaryData
 
@@ -17,12 +17,24 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }))
 }
 
-const addDiary = () => {
-  return null
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id)
+  return entry
+}
+
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  }
+
+  diaries.push(newDiaryEntry)
+  return newDiaryEntry
 }
 
 export default {
   getEntries,
   getNonSensitiveEntries,
   addDiary,
+  findById,
 }
--- part9/ts-flight-diary/src/types.ts
+++ part9/ts-flight-diary/src/types.ts
@@ -1,6 +1,10 @@
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
-
-export type Visibility = 'great' | 'good' | 'ok' | 'poor'
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
+}
 
 export interface DiaryEntry {
   id: number
@@ -10,4 +14,13 @@ export interface DiaryEntry {
   comment?: string
 }
 
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
+
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>
+
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
